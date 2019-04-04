e2f184a54f9657a5867201dca3848d87547fbe1c
akomykel <akomykel@gmail.com>
2019-04-04 11:22:13 +0800

    Mykel: removing old wp files

--- wp-content/themes/dgcdavao_theme/functions.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: root
- * Date: 12/14/17
- * Time: 4:58 PM
- */
--- wp-content/themes/dgcdavao_theme/header.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: root
- * Date: 12/14/17
- * Time: 4:57 PM
- */
